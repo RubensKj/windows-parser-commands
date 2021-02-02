72457f31f7f1a3c9fb6052026dfe6dbf3a4670eb
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-02-01 23:11:28 -0300

    Changing upload and download

--- Network/download.bat
+++ Network/download.bat
@@ -6,14 +6,16 @@ IF "%2"=="" GOTO ERROR_FILE_NAME
 
 ECHO Download arquive %2 with curl. URL: %1
 curl %1 -o %2
-@ECHO ON
-ECHO Downloaded..
-@ECHO OFF
-PAUSE
-EXIT
+GOTO FINISH
 
 :ERROR_LINK
 ECHO Link must be specified
 
 :ERROR_FILE_NAME
 ECHO File name must be specified
+
+:FINISH
+@ECHO ON
+ECHO Downloaded..
+@ECHO OFF
+PAUSE
--- Network/upload.bat
+++ Network/upload.bat
@@ -4,12 +4,13 @@ IF "%1"=="" GOTO ERROR_FILE
 
 ECHO Uploading arquive %1 with curl
 curl --upload-file "%1" https://transfer.sh/"%1"
+GOTO FINISH
+
+:ERROR_FILE
+ECHO File must be specified
+
+:FINISH
 @ECHO ON
 ECHO Uploaded..
 @ECHO OFF
 PAUSE
-EXIT
-
-
-:ERROR_FILE
-ECHO File must be specified
